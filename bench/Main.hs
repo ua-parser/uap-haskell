e15bfd20dd0baf249791d8712b383f6969828e67
Michael Xavier <michael@michaelxavier.net>
2015-11-18 11:37:55 -0800

    Benchmark all 3 types of parsers

--- bench/Main.hs
+++ bench/Main.hs
@@ -6,6 +6,7 @@ module Main
 
 
 -------------------------------------------------------------------------------
+import           Control.Applicative
 import           Control.DeepSeq
 import           Criterion.Main
 import           Data.DeriveTH
@@ -17,10 +18,13 @@ import           Web.UAParser.SuiteUtils
 
 main :: IO ()
 main = do
-  cases <- loadTests "test_resources/firefox_user_agent_strings.yaml"
-  let ua = bench "UA Parsing" $ nf (map (parseUA . uatcString)) cases
-  print $ show (length cases) ++ " strings being parsed."
-  defaultMain [ua]
+  uas <- take 100 <$> loadTests "test_resources/firefox_user_agent_strings.yaml"
+  oses <- take 100 <$> loadTests "test_resources/additional_os_tests.yaml"
+  devs <- take 100 <$> loadTests "tests/test_device.yaml"
+  defaultMain [ bench "Parsing 100 UAs" $ nf (map (parseUA . uatcString)) uas
+              , bench "Parsing 100 OSes" $ nf (map (parseOS . ostcString)) oses
+              , bench "Parsing 100 Devices" $ nf (map (parseDev . dtcString)) devs
+              ]
 
 
-$(derives [makeNFData] [''UAResult])
+$(derives [makeNFData] [''UAResult, ''OSResult, ''DevResult])
